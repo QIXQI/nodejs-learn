e3e1e94d661667407ad04533cc2f4ad91f3f6468
qixqi <zhengxiang4056@gmail.com>
2020-03-02 10:47:58 +0800

    https

--- node深入浅出/https.js
+++ node深入浅出/https.js
@@ -1,3 +1,6 @@
+// 卧槽，太奇怪了，在阿里云ubuntu服务器中，443、80端口需要在root下执行 node https.js 才行，否则报端口占用的error
+// 其他端口在普通用户下执行就可以
+
 const https = require('https');
 const fs = require('fs');
 
@@ -10,6 +13,6 @@ const options = {
 https.createServer(options, (req, res) => {
     res.writeHead(200);
     res.end('Hello World!\n');
-}).listen(8080, function(){
-    console.log('visit https://www.ourvultr.club:8080');
+}).listen(443, function(){
+    console.log('visit https://www.ourvultr.club');     
 });
