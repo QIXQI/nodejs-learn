1d79b64df2077c3784f450977ac875c3618bc232
qixqi <zhengxiang4056@gmail.com>
2020-03-03 23:15:19 +0800

    学习express框架

--- express_demo.js
+++ express_demo.js
@@ -1,5 +1,10 @@
 const express = require('/Users/zhengxiang/.nvm/versions/node/v10.10.0/lib/node_modules/express');
+const bodyParser = require('/Users/zhengxiang/.nvm/versions/node/v10.10.0/lib/node_modules/body-parser');
 var app = express();
+
+
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 
 app.use('/static', express.static('static'));
@@ -32,6 +37,35 @@ app.get('/list_user', (req, res) => {
 app.get('/ab*cd', (req, res) => {
     res.send('正则匹配');
     console.log('[GET] - /ab*cd 请求');
+});
+
+
+// 传送到静态文件
+// http://127.0.0.1:8081/index.htm
+app.get('/index.htm', (req, res) => {
+    res.sendFile(__dirname + '/' + 'express_demo.htm');
+});
+
+
+
+// http://127.0.0.1:8081/process_get
+app.get('/process_get', (req, res) => {
+    var response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+}); 
+
+
+app.post('/process_post', urlencodedParser, (req, res) => {
+    var response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 });
 
 
